0e2c3bba7f9bc8567f0f678b18aca5e943f7fe83
seren <srndayi90@gmail.com>
2023-09-27 16:51:48 +0200

    oddEven task solution

---
+++ src/week1_codingTasks/odd_Even.java
@@ -0,0 +1,22 @@
+package week1_codingTasks;
+
+public class odd_Even {
+    public static void main(String[] args) {
+
+        int x = 15;
+
+        if (x%2 == 0){
+            System.out.println("Even number");
+        }else{
+            System.out.println("Odd number");
+        }
+
+
+
+    }
+}
+/*
+Numbers- odd & even
+    Write a method which can identifies given number is even or odd
+ */
+
