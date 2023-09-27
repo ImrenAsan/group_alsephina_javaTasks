a62c219c7dd6ff2da1a74868110a792c28bd74db
ImrenAsan <imrenasan@protonmail.com>
2023-09-27 21:00:49 +0200

    imren -- week01 task02 added

---
+++ src/week1_codingTasks/TASK2_Imren.java
@@ -0,0 +1,15 @@
+package week1_codingTasks;
+
+public class TASK2_Imren {
+
+    public static void main(String[] args) {
+
+
+    }
+}
+
+/*
+Numbers -- Divide without / operator
+write a method that can divide two numbers without using division operator
+
+ */
